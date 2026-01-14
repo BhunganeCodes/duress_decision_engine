bbf766b1f1eee600e13a9929b15ad28f05e0080a
BhunganeCodes <thamsanqahadebe98@gmail.com>
2026-01-14 11:09:46 +0200

    Update: Added a bit more logic into the risk analysis method for the duress decision engine

--- duress_engine.py
+++ duress_engine.py
@@ -1,14 +1,24 @@
-
+# Duress Decision Engine Logic Concept Simplistic Version.
 
 class DuressDecisionEngine:
 
     def __init__(self):
         pass
 
-    def risk_analysis(self, pin, duress_pin):
+    def risk_analysis(self, pin, requested_amount):
+        duress_pin = int(str(pin)[::-1])
+
         if pin == duress_pin:
-            return {
+            response = {
+                "approved": True,
+                "dispense_amount": 200,
                 "silent_alert": True,
-                "decreased_balance": True,
-                "status": "APPROVED"
+                "flag": "DURESS_EVENT"
             }
+        else:
+            response = {
+                "approved": True,
+                "dispense_amount": requested_amount,
+                "silent_alert": False
+            }
+        return response
